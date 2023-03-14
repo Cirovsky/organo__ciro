b6697979fbbe6a63a78a35de5c937aee9be33877
Cirovsky <cirovisqui@gmail.com>
2023-03-14 20:04:48 -0300

    criada comunicação entre Form e Container

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,19 +1,45 @@
-import "./index.css"
+import "./Container.css"
 import Time from "../Time";
 
 function Container(props){
-    console.log(props);
     class time{
         constructor(titulo){
             this.titulo = titulo;
-            this.id = titulo;
+            this.id = this.normalizar(titulo);
             this.arrayCard = [];
         }
-
+        normalizar(texto){
+            return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().split(" ").join("");
+        }
     }
     const arrayTime =[];
-    const titulosTime = arrayTime.map(time => time.titulo);
-    if(props.card){
+    const cards = localStorage.length > 0? JSON.parse(localStorage.cards): "";
+
+    function initCards(){
+        let titulosTime = []
+        if(arrayTime.length > 0){
+            titulosTime = arrayTime.map(time => time.titulo);
+        }else{
+            titulosTime.push(cards[0].time);
+            arrayTime.push(new time(cards[0].time));
+        }
+        cards.forEach(card =>{
+            if(titulosTime.includes(card.time)){
+                const index = titulosTime.indexOf(card.time);
+                arrayTime[index].arrayCard.push(card);
+            }else{
+                titulosTime.push(card.time);
+                arrayTime.push(new time(card.time));
+                arrayTime[arrayTime.length -1].arrayCard.push(card);
+            }
+        })
+        
+    }
+    if(cards != ""){
+        initCards();
+    }
+    
+    /* if(props.card){
         if(titulosTime.includes(props.card.time)){
             const index = titulosTime.indexOf(props.card.time);
             arrayTime[index].arrayCard.push(props.card);
@@ -21,11 +47,11 @@ function Container(props){
             arrayTime.push(new time(props.card.time));
             arrayTime[arrayTime.length -1].arrayCard.push(props.card);
         }
-    }
+    } */
 
     return(
         <div className="container">
-            {arrayTime.map( time => <Time titulo={time.titulo} arrayCard={time.arrayCard}/>)}
+            {arrayTime.map( time => <Time titulo={time.titulo} id={time.id} arrayCard={time.arrayCard}/>)}
         </div>
     )
 }
--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -1,15 +1,31 @@
 import Card from "../Card";
-import './Time.css'
+import './Time.css';
+import { corTime } from "./coresTimes";
 
 const Time = (props) => {
-    const arrayCard = props.arrayCard;
     
+    console.log(props);
+    const arrayCard = props.arrayCard;
+    let destaque = "";
+    let fundo = "";
+    corTime.forEach( cor =>{
+        if(cor.nome == props.id){
+            destaque = cor.destaque;
+            fundo = cor.fundo;
+        }
+    }
+
+    )
+           
         return (
-        <div className="container__time" id={props.id}>
+        <div className="container__time" id={props.id} style={{backgroundColor: fundo}}>
             <h2>{props.titulo}</h2>
+            <div className="tracinho__time" style={{backgroundColor: destaque}}></div>
             <ul className="card__setor__lista" key={props.titulo}>
-                {arrayCard.map(card => <Card nome={card.nome} cargo={card.cargo}/>)}
+                {arrayCard.map(card => <Card nome={card.nome} cargo={card.cargo} corDestaque={destaque}/>)}
             </ul>
+
+
         </div>
     )
 }
